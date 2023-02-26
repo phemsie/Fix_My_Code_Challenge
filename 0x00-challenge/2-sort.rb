cf546a487d6e3b97c40b1068db204a67294a95ba
phemsie <femi_oworu@yahoo.com>
2023-02-26 15:12:22 +0100

    Fix_My_Code_Challenge

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -1,6 +1,6 @@
 ###
 #
-#  Sort integer arguments (ascending) 
+#  Sort integer arguments (ascending)
 #
 ###
 
@@ -11,18 +11,19 @@ ARGV.each do |arg|
 
     # convert to integer
     i_arg = arg.to_i
-    
+
     # insert result at the right position
     is_inserted = false
     i = 0
     l = result.size
     while !is_inserted && i < l do
         if result[i] < i_arg
+            i += 1
+        else
             result.insert(i, i_arg)
             is_inserted = true
             break
         end
-        i += 1
     end
     result << i_arg if !is_inserted
 end
